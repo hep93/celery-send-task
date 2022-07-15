96ecd52dd358093c6af8ac8e778e5fc206a60c04
hep93 <hep93@naver.com>
2022-07-15 13:54:59 +0900

    Add progress bar example

--- ex2-result-backend/send_task.py
+++ ex2-result-backend/send_task.py
@@ -1,12 +1,24 @@
 from celery import Celery
 import time
 
+def progress_bar(current, total):
+    bar = ''
+    for i in range(total):
+        if i <= current:
+            bar += '⬛'
+        else:
+            bar += '⬜'
+    return bar
+
 celery = Celery()
 celery.conf.update({ 'broker_url': 'amqp://guest:guest@localhost:5672//', 'result_backend': 'rpc://' })
 result = celery.send_task('task_wait')
+print(f'Status: {result.status}')
 
 while not result.ready():
     time.sleep(1)
-    print('Waiting for result...')
+    print(f'Waiting for result... Status: {result.status}')
+    if result.status == 'PROGRESS':
+        print(progress_bar(result.info['current'], result.info['total']))
 
 print(f'Result: {result.get()}')
--- ex2-result-backend/workers/tasks.py
+++ ex2-result-backend/workers/tasks.py
@@ -2,9 +2,10 @@ from workers.config import app
 from func.thumbnail import create_thumbnail
 import time
 
-@app.task(bind=True, name='task_wait')
+@app.task(bind=True, name='task_wait', track_started=True)
 def wait(self):
     for i in range(10):
         print(f'Waiting.... {i} sec')
         time.sleep(1)
+        self.update_state(state='PROGRESS', meta={'current': i, 'total': 10})
     return 'This is a result!'
