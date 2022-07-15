63a935b174a409b7b5f11761afc46130925b3d00
hep93 <hep93@naver.com>
2022-07-15 13:43:31 +0900

    chore: change name thumbnail.py -> tasks.py

--- ex1-send-task/workers/tasks.py
+++ ex1-send-task/workers/tasks.py
@@ -1,6 +1,6 @@
 from workers.config import app
 from func.thumbnail import create_thumbnail
 
-@app.task(bind=True, name='create_thumbnail')
+@app.task(bind=True, name='create_thumbnail', ignore_result=True)
 def create(self, url, filename):
     create_thumbnail(url, filename)
--- func/thumbnail.py
+++ func/thumbnail.py
@@ -7,9 +7,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 def create_thumbnail(url, filename):
+    filepath = f'{os.environ.get("STATIC_DIR")}/{filename}' 
     logger.info('Begin creation of thumbnail')
     content = requests.get(url).content
     with Image.open(BytesIO(content)) as img:
         img.thumbnail((128, 128))
-        img.save(f'{os.environ.get("STATIC_DIR")}/{filename}')
+        img.save(filepath)
     logger.info('Finished creation of thumbnail')
+    return filepath
